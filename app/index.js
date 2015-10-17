0e59d0d2db9c4277ef74a9df66c1df3a63f1200a
Christophe Hurpeau <christophe@hurpeau.com>
2015-10-18 01:05:02 +0200

    fix license name

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ module.exports = generators.Base.extend({
     configuring: function() {
         this.spawnCommandSync('npm', ['init']);
         this.fs.copy(this.templatePath('Makefile'), this.destinationPath('Makefile'));
-        this.fs.copy(this.templatePath('LICENCE'), this.destinationPath('LICENCE'));
+        this.fs.copy(this.templatePath('LICENSE'), this.destinationPath('LICENSE'));
         this.mkdir('src');
         this.mkdir('lib');
     },

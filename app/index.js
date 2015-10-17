32ac7236cd38a504c3fbda27dd1cafb3c1c4d0d6
Christophe Hurpeau <christophe@hurpeau.com>
2015-10-18 00:48:15 +0200

    LICENCE and tasks order

--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-    install: function () {
+
+    configuring: function() {
         this.spawnCommandSync('npm', ['init']);
-        this.npmInstall(['springbokjs-library'], { 'saveDev': true });
-        this.npmInstall(['babel-runtime'], { 'saveDev': false });
         this.fs.copy(this.templatePath('Makefile'), this.destinationPath('Makefile'));
+        this.fs.copy(this.templatePath('LICENCE'), this.destinationPath('LICENCE'));
         this.mkdir('src');
         this.mkdir('lib');
+    },
+
+    install: function () {
+        this.npmInstall(['springbokjs-library'], { 'saveDev': true });
+        this.npmInstall(['babel-runtime'], { 'saveDev': false });
+    },
+
+    end: function() {
         this.spawnCommandSync('make', ['install-config-files', 'install-scripts']);
     }
 });
